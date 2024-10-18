9f8020b551e2cc54f3e5260ad1b317a9d105f626
Jonathan <jonsouyang@gmail.com>
2024-10-18 15:39:49 -0700

    environment init

--- playground.py
+++ playground.py
@@ -3,11 +3,11 @@ print("executing file")
 # in order to execute code live, pass the code in as a string to a variable and use the python exec() function to run it
 
 code = """
-def counter(n):
-    for i in range(n):
-        print(i)
-        
-counter(5)
+print('test 1')
 """
 
-exec(code)
+code2 = """
+print('test 2)
+"""
+
+exec(code + code2)
