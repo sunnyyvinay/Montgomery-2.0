6e7d378422a3232a63f52dae0293847ea228bdf5
Jonathan <jonsouyang@gmail.com>
2024-10-18 17:50:24 -0700

    random changes (unimportant)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from gemini_call import call_gemini
+#from gemini_call import call_gemini
 from external_functions import speak_to_user, animate_with_manim
 
-print(animate_with_manim('test'))
+animate_with_manim('test')
+speak_to_user('test')
