06721f1416d7a9efdf8664fe4a291f1bc8e934dc
Jonathan <jonsouyang@gmail.com>
2024-10-20 03:19:51 -0700

    backend bug fixes

--- fastapi/api/manim_script.py
+++ fastapi/api/manim_script.py
@@ -1,6 +1,13 @@
 from manim import *
 class video(Scene):
     def construct(self):
-        line1 = MathTex("y = x^2").shift(UP * 2)
-        self.play(Write(line1))
+        axes = Axes(
+            x_range=[-5, 5, 1],
+            y_range=[-5, 5, 1],
+            x_length=8,
+            y_length=6,
+        )
+        graph = axes.plot(lambda x: x, color=RED)
+        eq = MathTex("y=x").scale(2).next_to(axes,UP)
+        self.play(Create(axes),Create(graph),Write(eq))
 
